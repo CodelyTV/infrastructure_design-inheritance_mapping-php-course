44591092304b07882560061f283e0c3620bc67b4
Rafa Gómez <rgomezcasas@gmail.com>
2023-12-20 16:41:32 +0100

    feat(03-2): add script to fill data

--- 03-problems_with_inheritance_mapping/2-performance/fill_tables.sh
+++ 03-problems_with_inheritance_mapping/2-performance/fill_tables.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
 DB_NAME="mooc"
-NUM_ROWS=1000
+NUM_ROWS=10000
+BATCH_SIZE=500
+
+declare -a STEPS_MAIN
+declare -a STEPS_VIDEO
+declare -a STEPS_EXERCISE
+declare -a STEPS_QUIZ
+declare -a STEPS_INLINE
+
+function join_by {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
 
 for i in $(seq 1 $NUM_ROWS)
 do
@@ -27,24 +40,44 @@ do
   TITLE="Titulo $i"
   DURATION=$(($RANDOM % 120 + 1))
 
-  docker exec -it codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps (id, title, duration, type) VALUES ('$ID', '$TITLE', $DURATION, '$TYPE');"
+  STEPS_MAIN+=("('$ID', '$TITLE', $DURATION, '$TYPE')")
 
   case $TYPE in
     "video")
       URL="http://video$i.com"
-      docker exec -it codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_video (id, url) VALUES ('$ID', '$URL');"
+      STEPS_VIDEO+=("('$ID', '$URL')")
       ;;
     "exercise")
       CONTENT="Contenido $i"
-      docker exec -it codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_exercise (id, content) VALUES ('$ID', '$CONTENT');"
+      STEPS_EXERCISE+=("('$ID', '$CONTENT')")
       ;;
     "quiz")
       QUESTIONS="Pregunta $i"
-      docker exec -it codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_quiz (id, questions) VALUES ('$ID', '$QUESTIONS');"
+      STEPS_QUIZ+=("('$ID', '$QUESTIONS')")
       ;;
   esac
 
-  docker exec -it codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_inline (id, title, duration, type, url, content, questions) VALUES ('$ID', '$TITLE', $DURATION, '$TYPE', '$URL', '$CONTENT', '$QUESTIONS');"
+  STEPS_INLINE+=("('$ID', '$TITLE', $DURATION, '$TYPE', '$URL', '$CONTENT', '$QUESTIONS')")
 
-  echo "Step $i/$NUM_ROWS inserted"
+  if [ $((i % BATCH_SIZE)) -eq 0 ] || [ $i -eq $NUM_ROWS ]; then
+    MAIN_VALUES=$(join_by , "${STEPS_MAIN[@]}")
+    VIDEO_VALUES=$(join_by , "${STEPS_VIDEO[@]}")
+    EXERCISE_VALUES=$(join_by , "${STEPS_EXERCISE[@]}")
+    QUIZ_VALUES=$(join_by , "${STEPS_QUIZ[@]}")
+    INLINE_VALUES=$(join_by , "${STEPS_INLINE[@]}")
+
+    docker exec -i codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps (id, title, duration, type) VALUES $MAIN_VALUES;"
+    [ ${#STEPS_VIDEO[@]} -ne 0 ] && docker exec -i codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_video (id, url) VALUES $VIDEO_VALUES;"
+    [ ${#STEPS_EXERCISE[@]} -ne 0 ] && docker exec -i codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_exercise (id, content) VALUES $EXERCISE_VALUES;"
+    [ ${#STEPS_QUIZ[@]} -ne 0 ] && docker exec -i codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_quiz (id, questions) VALUES $QUIZ_VALUES;"
+    docker exec -i codely-infra_modeling_inheritance_mapping-performance mariadb $DB_NAME -e "INSERT INTO steps_inline (id, title, duration, type, url, content, questions) VALUES $INLINE_VALUES;"
+
+    STEPS_MAIN=()
+    STEPS_VIDEO=()
+    STEPS_EXERCISE=()
+    STEPS_QUIZ=()
+    STEPS_INLINE=()
+
+    echo "Steps inserted up to $i/$NUM_ROWS"
+  fi
 done
